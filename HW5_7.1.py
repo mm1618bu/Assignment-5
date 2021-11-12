d409a739b0b0a2cc042a93359154b2bca1ee0871
mm1618bu <90989929+mm1618bu@users.noreply.github.com>
2021-11-11 21:28:07 -0500

    Create HW5_7.1.py

---
+++ HW5_7.1.py
@@ -0,0 +1,25 @@
+#! /usr/bin/python3
+
+class Rectangle:
+	def __init__(self,width,height):
+		self.width = width
+		self.height = height
+
+	def getArea(self):
+		return self.width * self.height
+
+	def getPerimeter(self):
+		return(self.height + self.height) * 2
+
+def main():
+    width1, height1 = 4,40
+    width2, height2 = 3.5,35.7
+    rect1 = Rectangle(width1, height1)
+    rect2 = Rectangle(width2, height2)
+
+    print("Calculations for rectangle one: ")
+    print(" Width: ",width1,'\n'," Height: ",height1,'\n'," Area: ",rect1.getArea(),'\n'," Perimeter: ", rect1.getPerimeter())
+    print("Calculations for rectangle two: ")
+    print(" Width: ",width2,'\n'," Height: ",height2,'\n'," Area: ",rect2.getArea(),'\n', " Perimeter: ", rect2.getPerimeter())
+
+main()
