a29f6e22974cac75871491418776f54fcc7f4f36
mm1618bu <90989929+mm1618bu@users.noreply.github.com>
2021-11-21 22:09:53 -0500

    Update HW5_7.6.py

--- HW5_7.6.py
+++ HW5_7.6.py
@@ -1,28 +1,33 @@
 import math
 
+# Define fuction variables
 a = int(input(" A: "))
 b = int(input(" B: "))
 c = int(input(" C: "))
 
 class QuadraticEquation:
+    # Initialize Variables
     def __init__(self, a, b, c):
         self.a = a
         self.b = b
         self.c = c
+    # Return the Discriminant
     def getDiscriminant(self):
         return ((self.b)**2) - (4 * self.a * self.c)
-
+    # Find the first root
     def getRoot1(self):
         if self.getDiscriminant() < 0:
             return None
         else:
             return (-self.b + math.sqrt(self.getDiscriminant())) / (2 * self.a)
+    # Find the second root
     def getRoot2(self):
         if self.getDiscriminant() < 0:
             return None
         else:
             return (-self.b - math.sqrt(self.getDiscriminant())) / (2 * self.a)
 
+# Final Answer
 k = QuadraticEquation(a,b,c)
 print(k.getRoot1())
 print(k.getRoot2())
